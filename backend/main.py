4ead545a6c4ba5393d9b15ee9de4b72afb6efdbb
vaniyarehan <vrehan33@gmail.com>
2025-08-25 22:32:34 +0500

    updated frontend url for cors

--- backend/main.py
+++ backend/main.py
@@ -60,6 +60,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://bubulfriend-frontend.up.railway.app",
         "https://bulbulfriend-frontend.up.railway.app",  
     ],
     allow_credentials=True,
