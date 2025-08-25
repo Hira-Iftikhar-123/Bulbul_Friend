f1d4d13ce25dffe35775c9e30cfa44773b3f3639
vaniyarehan <vrehan33@gmail.com>
2025-08-25 22:42:39 +0500

    updated frontend url in middleware

--- backend/main.py
+++ backend/main.py
@@ -60,7 +60,6 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://bubulfriend-frontend.up.railway.app",
         "https://bulbulfriend-frontend.up.railway.app",  
     ],
     allow_credentials=True,
